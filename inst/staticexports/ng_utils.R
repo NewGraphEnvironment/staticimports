ba02f8542b8ce136e3cd1a506dfdc807da6f3620
almac2022 <al@newgraphenvironment.com>
2025-01-13 12:25:01 -0800

    add sngu_round

--- inst/staticexports/ng_utils.R
+++ inst/staticexports/ng_utils.R
@@ -11,3 +11,9 @@ sngu_row_empty_index <- function(dat, position = 1){
     dplyr::pull(index) |>
     head(position)
 }
+
+# this is necessary to get around R's weird rounding where 1.55 would round to 1.5 vs 1.6 as we would expect.
+sngu_round <- function(x, decimal_places = 1) {
+  multiplier <- 10^decimal_places
+  floor(x * multiplier + 0.5) / multiplier
+}
