20fdcf505b091d52163ee949eedbbc6916235046
almac2022 <al@newgraphenvironment.com>
2025-11-21 14:51:53 -0800

    sfis_tab_sp to close #20

---
+++ inst/staticexports/ng_fis.R
@@ -0,0 +1,94 @@
+##get the observations from the fiss layer
+sfis_tab_sp <- function(
+    wsg_code = NULL,
+    burn = TRUE,
+    path_out = 'data/inputs_extracted/fiss_species_table.csv'
+    ){
+  conn <- fpr::fpr_db_conn()
+  fish_species_watershed <- fpr::fpr_db_query(query = glue::glue_sql("SELECT DISTINCT ws.watershed_group_code, x.species_code,x.species_name
+                   FROM whse_fish.fiss_fish_obsrvtn_pnt_sp x
+                   INNER JOIN
+                   whse_basemapping.fwa_watershed_groups_poly ws
+                   ON ST_intersects(x.geom, ws.geom)
+                   WHERE ws.watershed_group_code IN ({wsg_code*})",
+                                                                     .con = conn))
+
+  DBI::dbDisconnect(conn)
+
+  # split data frame into list of data frames based on watershed group code
+  fish_spp_prep <- fish_species_watershed |>
+    dplyr::group_split(watershed_group_code)
+
+  # grab watershed names from xref table - https://github.com/NewGraphEnvironment/rfp/issues/5
+  xref_wsg <- fpr::fpr_db_query(
+    query= "SELECT watershed_group_code, watershed_group_name
+  FROM whse_basemapping.fwa_watershed_groups_poly"
+  ) |>
+    dplyr::arrange(watershed_group_code)
+
+
+  wshd_names <- xref_wsg |>
+    dplyr::filter(watershed_group_code %in% wsg_code) |>
+    dplyr::pull(watershed_group_name) |>
+    # remove " River" from the names
+    stringr::str_remove(" River")
+
+  names_tbl <- c('species_name', 'species_code', wshd_names)
+
+  #merge all data frames in list
+  fish_spp <- fish_spp_prep |>
+    purrr::reduce(dplyr::full_join, by= c('species_code', 'species_name')) |>
+    dplyr::relocate(c(species_name, species_code), .before = everything()) |>
+    purrr::set_names(names_tbl)
+
+  fish_all <- fishbc::freshwaterfish
+  fish_cdc <- fishbc::cdc
+
+  fish_spp2 <- dplyr::left_join(fish_spp,
+                                fish_all,
+                                by = c("species_code" = "Code")) |>
+    dplyr::filter(!is.na(Class) & !species_code == 'TR') |> ##mottled sculpin has some sort of error going on
+    # mutate(CDCode = case_when(species_code == 'BT' ~ 'F-SACO-11', ##pacific population yo
+    #                           T ~ CDCode)) %>%
+    dplyr::select(species_code,
+                  species_name,
+                  dplyr::all_of(wshd_names),
+                  CDCode)
+
+  fish_spp3 <- dplyr::left_join(
+    fish_spp2,
+    fish_cdc,
+    by = c('CDCode' = 'Species Code')
+  ) |>
+    dplyr::select(`Scientific Name`,
+                  'Species Name' = species_name,
+                  # 'Species Code' = species_code,
+                  `BC List`,
+                  # `Provincial FRPA`,
+                  COSEWIC,
+                  # SARA,
+                  dplyr::all_of(wshd_names)
+    ) |>
+    dplyr::mutate(dplyr::across(dplyr::all_of(wshd_names), ~ifelse(!is.na(.), "Yes", .))) |>
+    dplyr::arrange(`Scientific Name`, `Species Name`)
+
+  ##print your table to input_raw for use in the report
+  if(burn){
+    fish_spp3 |>
+    readr::write_csv(file = path_out)
+  }else
+    fish_spp3
+}
+
+# wsg_code <- c(
+#   'LCHL',
+#   'NECR',
+#   'FRAN',
+#   # "MORK",
+#   "UFRA",
+#   'WILL',
+#   'TABR',
+#   'LSAL'
+# )
+#
+# t <- sfis_tab_sp(wsg_code = wsg_code, burn = FALSE)
